2bb16fb184fe0d37d9b28f1df44a3e0f5e7e5105
Jailene DeVine-Jones <jdevxv@gmail.com>
2022-01-07 20:37:40 -0500

    Fixed about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,21 +1,43 @@
 import React from "react";
 import "../Styles/About.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-import Jailene from "../Assets/Jailene.png"
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Jailene from "../Assets/Jailene.png";
 AOS.init();
 
 const About = () => {
   return (
     <div className="aboutBigDiv" id="about">
-      <h1 className="aboutTitle" data-aos="fade-down">About Me</h1>
+      <h1 className="aboutTitle" data-aos="fade-down">
+        About Me
+      </h1>
       <div className="aboutContainer">
-        <img className="jailene" src={Jailene} alt="Jailene DeVine-Jones" data-aos="fade-right"/>
+        <img
+          className="jailene"
+          src={Jailene}
+          alt="Jailene DeVine-Jones"
+          data-aos="fade-right"
+        />
         <p className="aboutText" data-aos="fade-left">
-          This is my about me. I have no idea what to put here at the moment but
-          it is something. Talk about JS, React, all that good stuff. Maybe even
-          talk about how I like to game and play sudoku and tetris on my
-          freetime. And add an image of myself.
+          Programming has always been a passion of mine. Fascinated by the world
+          of video games, my journey began at a young age when I would research
+          game design and the different working aspects that come together for
+          users to enjoy. <br/> <br/>
+          
+          This fascination led me to the wonderful world of
+          programming. After successfully gaining my CompTIA A+, CompTIA
+          Security+, and CompTIA Linux+ certifications from NPower, I became a
+          Tech Professional. I was also selected as a Fellow at Pursuit, an
+          intensive 12-month software engineering fellowship with a 9%
+          acceptance rate, where I have built my repertoire in HTML,
+          CSS, JavaScript, and many more. My end goal is to become a video game
+          programmer! <br/> <br/>
+          
+          When I am not working or learning, I enjoy completing
+          Sudoku puzzles to keep my mind active as well as, of course, video
+          games such as Tetris and Rocket League! If you're
+          interested in learning more about me, or joining my Rocket League
+          team, feel free to send me an email!
         </p>
       </div>
     </div>
