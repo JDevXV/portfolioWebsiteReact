19e947fa66086722bb25a6d51feb4259e55727ed
Jailene DeVine-Jones <jdevxv@gmail.com>
2022-01-08 21:16:19 -0500

    Added React Carousel

---
+++ src/Components/Carousel.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import calcDemo from "../Assets/Calculator.png";
+import givnDemo from "../Assets/Givn.png"
+
+const CarouselComponent = () => {
+  return (
+    <div>
+      <Carousel infiniteLoop="true">
+        <div className="givnDemo">
+          <div className="givnDemoDesc">
+            <h2>GIVN</h2>
+            <h4>JavaScript, HTML, CSS, React, Redux, PostgreSQL</h4>
+            <p>
+              Givn is an educational app that encourages and incentivizes users
+              <br /> to give away their unwanted goods instead of throwing them
+              away.
+            </p>
+            <div className="givnDemoLinks">
+              <button
+                className="buttonLink"
+                href="https://givn-application.web.app"
+              >
+                Live
+              </button>
+              <button
+                className="buttonLink"
+                href="https://github.com/JDevXV/Capstone-Givn"
+              >
+                Github
+              </button>
+            </div>
+          </div>
+          <div className="givnDemoGif">
+            <img src={givnDemo} alt="GIVN Demo" />
+          </div>
+        </div>
+        <div className="calcDemo">
+          <div className="calcDemoDesc">
+            <h2>Neon Calculator</h2>
+            <h4>JavaScript, HTML, CSS, React</h4>
+            <p>A simple calculator app with an awesome neon glowing affect.</p>
+            <div className="calcDemoLinks">
+              <button
+                className="buttonLink"
+                href="https://jdevxv-calculator.netlify.app"
+              >
+                Live
+              </button>
+              <button
+                className="buttonLink"
+                href="https://github.com/JDevXV/FSW-React-Calculator"
+              >
+                Github
+              </button>
+            </div>
+          </div>
+          <div className="calcDemoGif">
+            <img src={calcDemo} alt="Calculator Demo" />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselComponent;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,66 +2,14 @@ import React from "react";
 import "../Styles/Projects.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import calcDemo from "../Assets/Calculator.png";
-import givnDemo from "../Assets/Givn.png"
+import CarouselComponent from "./Carousel.js"
 AOS.init();
 
 const Projects = () => {
   return (
     <div className="projectsDiv" id="projects">
       <h1>Projects</h1>
-      <div className="givnDemo">
-        <div className="givnDemoDesc">
-          <h2>GIVN</h2>
-          <h4>JavaScript, HTML, CSS, React, Redux, PostgreSQL</h4>
-          <p>
-            Givn is an educational app that encourages and incentivizes users
-            <br /> to give away their unwanted goods instead of throwing them
-            away.
-          </p>
-          <div className="givnDemoLinks">
-            <button
-              className="buttonLink"
-              href="https://givn-application.web.app"
-            >
-              Live
-            </button>
-            <button
-              className="buttonLink"
-              href="https://github.com/JDevXV/Capstone-Givn"
-            >
-              Github
-            </button>
-          </div>
-        </div>
-        <div className="givnDemoGif">
-          <img src={givnDemo} alt="GIVN Demo" />
-        </div>
-      </div>
-      <div className="calcDemo">
-        <div className="calcDemoDesc">
-          <h2>Neon Calculator</h2>
-          <h4>JavaScript, HTML, CSS, React</h4>
-          <p>A simple calculator app with an awesome neon glowing affect.</p>
-          <div className="calcDemoLinks">
-            <button
-              className="buttonLink"
-              href="https://jdevxv-calculator.netlify.app"
-            >
-              Live
-            </button>
-            <button
-              className="buttonLink"
-              href="https://github.com/JDevXV/FSW-React-Calculator"
-            >
-              Github
-            </button>
-          </div>
-        </div>
-        <div className="calcDemoGif">
-          <img src={calcDemo} alt="Calculator Demo" />
-        </div>
-      </div>
+      <CarouselComponent/>
     </div>
   );
 };
